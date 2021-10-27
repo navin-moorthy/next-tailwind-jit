6c978faadf3570aeb5d1f2a74a3074346bee1614
Navin Moorthy <navin007.a@gmail.com>
2021-10-27 13:22:23 +0530

    feat(badge): ✨ add badge component

--- components/index.js
+++ components/index.js
@@ -1,9 +1,183 @@
 import * as React from "react";
 
-import { Button as RenderlesskitButton } from "@renderlesskit/react-tailwind";
+import { Badge } from "@renderlesskit/react-tailwind";
 
-export const Button = (props) => {
-  return <RenderlesskitButton {...props}>Continues</RenderlesskitButton>;
+export const BadgeStack = (props) => {
+  return (
+    <div className='flex flex-col space-y-4'>
+      <div className='flex flex-col space-y-1'>
+        <div className='space-x-2'>
+          <Badge {...props} size='sm' variant='solid' themeColor='default'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='subtle' themeColor='default'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='outline' themeColor='default'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='sm' variant='solid' themeColor='primary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='subtle' themeColor='primary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='outline' themeColor='primary'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='sm' variant='solid' themeColor='secondary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='subtle' themeColor='secondary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='outline' themeColor='secondary'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='sm' variant='solid' themeColor='success'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='subtle' themeColor='success'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='outline' themeColor='success'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='sm' variant='solid' themeColor='danger'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='subtle' themeColor='danger'>
+            Beta
+          </Badge>
+          <Badge {...props} size='sm' variant='outline' themeColor='danger'>
+            Beta
+          </Badge>
+        </div>
+      </div>
+      <div className='flex flex-col space-y-1'>
+        <div className='space-x-2'>
+          <Badge {...props} size='md' variant='solid' themeColor='default'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='subtle' themeColor='default'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='outline' themeColor='default'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='md' variant='solid' themeColor='primary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='subtle' themeColor='primary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='outline' themeColor='primary'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='md' variant='solid' themeColor='secondary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='subtle' themeColor='secondary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='outline' themeColor='secondary'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='md' variant='solid' themeColor='success'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='subtle' themeColor='success'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='outline' themeColor='success'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='md' variant='solid' themeColor='danger'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='subtle' themeColor='danger'>
+            Beta
+          </Badge>
+          <Badge {...props} size='md' variant='outline' themeColor='danger'>
+            Beta
+          </Badge>
+        </div>
+      </div>
+      <div className='flex flex-col space-y-1'>
+        <div className='space-x-2'>
+          <Badge {...props} size='lg' variant='solid' themeColor='default'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='subtle' themeColor='default'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='outline' themeColor='default'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='lg' variant='solid' themeColor='primary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='subtle' themeColor='primary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='outline' themeColor='primary'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='lg' variant='solid' themeColor='secondary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='subtle' themeColor='secondary'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='outline' themeColor='secondary'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='lg' variant='solid' themeColor='success'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='subtle' themeColor='success'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='outline' themeColor='success'>
+            Beta
+          </Badge>
+        </div>
+        <div className='space-x-2'>
+          <Badge {...props} size='lg' variant='solid' themeColor='danger'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='subtle' themeColor='danger'>
+            Beta
+          </Badge>
+          <Badge {...props} size='lg' variant='outline' themeColor='danger'>
+            Beta
+          </Badge>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Button;
+export default BadgeStack;
